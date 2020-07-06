8aa50a6ed0362ed0a9fb655c53e2ff4153fc9223
ihanadi <ismailhanadi41@gmail.com>
2020-07-06 22:49:21 +0300

    This project has been completed.

---
+++ script.js
@@ -0,0 +1,52 @@
+let api = {
+  key: '99212324d7afa248752d9c6c79699ea1',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+let searchInput = document.querySelector('.search-input input');
+
+searchInput.addEventListener('keypress', setQuery);
+
+function setQuery(eve){
+  if(eve.keyCode == 13){
+    getResults(searchInput.value)
+  }
+}
+
+function getResults(e){
+  fetch(`${api.base}weather?q=${e}&units=metric&APPID=${api.key}`)
+    .then(weather =>{
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(e){
+  console.log(e);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${e.name}, ${e.sys.country}`
+  
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerText = `${Math.floor(e.main.temp)}<span>°c</span>`;
+
+  let wetherMain = document.querySelector('.current .wether');
+  wetherMain.innerText = `${e.weather[0].main}`;
+
+  let hiLow = document.querySelector('.current .hi-low');
+  hiLow.innerText = `${Math.floor(e.main.temp_min)}°c / ${Math.floor(e.main.temp_max)}°c`;
+}
+
+function dateBuilder(d){
+  months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  let day = days[d.getDay()];
+  let month = months[d.getMonth()];
+  let date = d.getDate();
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
